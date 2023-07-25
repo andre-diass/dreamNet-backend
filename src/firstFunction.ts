cde33d65bc040d1282e79a0541c7b8185cf1cbbf
andre-diass <adsf@poli.br>
2023-07-25 16:16:51 -0300

    first commit

---
+++ src/firstFunction.ts
@@ -0,0 +1,17 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
