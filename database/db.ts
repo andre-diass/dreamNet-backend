8bb4f24d1ee362e572e5cc6629859187c25145a2
andre-diass <adsf@poli.br>
2023-07-26 16:35:04 -0300

    feat: install env plugin

--- database/db.ts
+++ database/db.ts
@@ -1,11 +1,9 @@
 import mongoose, { Model, Schema } from 'mongoose';
 
-const db = 'mongodb+srv://dreamNet-admin:mengonaveia@dreamnet-users.t0piljw.mongodb.net/?retryWrites=true&w=majority';
-
 let conn: typeof mongoose | null = null;
 export const connectDatabase = async function () {
   if (conn === null) {
-    conn = await mongoose.connect(db as string, {
+    conn = await mongoose.connect(process.env.DB as string, {
       serverSelectionTimeoutMS: 5000,
     });
   }
