749e1f4833bce1fc9f29df1078663f5d0beb828b
andre-diass <adsf@poli.br>
2023-08-30 16:45:52 -0300

    feat: implement product controller

--- src/functions/getProducts.ts
+++ src/functions/getProducts.ts
@@ -1,36 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { MongoClient } from 'mongodb';
+import { ProductController } from '../controllers/product.controller';
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  try {
-    const client = await MongoClient.connect(process.env.DB as string);
-    const db = client.db('test');
-    const collection = db.collection('products');
+export const handler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const param = _event.queryStringParameters?.userId;
+  const product = new ProductController();
 
-    const paramName = event.queryStringParameters?.userId;
-    const result = await collection.find({ userId: paramName }).toArray();
+  const response = product.getProducts(param);
 
-    client.close();
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST',
-      },
-      body: JSON.stringify(result),
-    };
-    return response;
-  } catch (err) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST',
-      },
-      body: 'An error occured',
-    };
-  }
+  return response;
 };
