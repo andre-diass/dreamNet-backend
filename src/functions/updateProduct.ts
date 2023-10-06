db20b19836c80e320b5832d6cd26190485fd2e74
andre-diass <adsf@poli.br>
2023-10-06 00:00:04 -0300

    feat: extend product entity properties

--- src/functions/updateProduct.ts
+++ src/functions/updateProduct.ts
@@ -6,14 +6,17 @@ import buildResponse from '../utils/buildResponse';
 export const handler: APIGatewayProxyHandler = async (event, context) => {
   const productId = event.queryStringParameters?.productId as string;
   const requestBody = event.body || '{}';
-  const { name, description, price } = JSON.parse(requestBody);
+  const { name, description, price, imageLinks } = JSON.parse(requestBody);
 
   const productObj: Partial<IProduct> = {
     name,
     description,
     price,
+    imageLinks,
   };
   const product = new ProductController();
+
+  console.log(imageLinks);
 
   try {
     const result = await product.updateProduct(productObj, productId);
