b786d9db3840d95f0e535f7b7c7d9c06d625e72e
andre-diass <adsf@poli.br>
2023-11-03 21:37:24 -0300

    feat: add post endpoint for category entity

---
+++ src/functions/addCategory.ts
@@ -0,0 +1,25 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { CategoryController } from '../controllers/category.controller';
+import { ICategory } from '../utils/_types';
+import buildResponse from '../utils/buildResponse';
+import { SuccessfullCodes, ClientErrorCodes } from '../utils/statusCode';
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const requestBody = event.body || '{}';
+  const { category } = JSON.parse(requestBody);
+  const categoryObj: ICategory = {
+    category,
+  };
+  const categoryController = new CategoryController();
+
+  try {
+    const result = await categoryController.createCategory(categoryObj);
+    const response = buildResponse.buildSuccessfullResponse(SuccessfullCodes.Created, result);
+    return response;
+  } catch (error) {
+    const failedResponse = buildResponse.buildErrorResponse(ClientErrorCodes.BadRequest, error);
+    return failedResponse;
+  }
+};
