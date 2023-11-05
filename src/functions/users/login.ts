fd0cb3968ab51d3d8ac738d5c2c64bbc3d3e995e
andre-diass <adsf@poli.br>
2023-11-05 10:48:11 -0300

    feat: add basic jwt issuer

--- src/functions/users/login.ts
+++ src/functions/users/login.ts
@@ -1,9 +1,17 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
 import jwt from 'jsonwebtoken';
+import buildResponse from '../../utils/buildResponse';
+import { SuccessfullCodes } from '../../utils/statusCode';
 
-const secret = 'ssshhh';
-const user = {
-  id: 123,
-  scopes: ['users:read'],
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  const secret = 'ssshhh';
+  const user = {
+    id: 123,
+    scopes: ['users:read'],
+  };
+  const token = jwt.sign({ user }, secret);
+  console.log(`JWT issued: ${token}`);
+
+  const response = buildResponse.buildSuccessfullResponse(SuccessfullCodes.Created, token);
+  return response;
 };
-const token = jwt.sign({ user }, secret);
-console.log(`JWT issued: ${token}`);
