ccd475f2ebbcb19ce170adf6193d910a132a4cc8
andre-diass <adsf@poli.br>
2023-11-13 20:16:17 -0300

    feat: add user data access

--- src/utils/_types.ts
+++ src/utils/_types.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 export interface IProduct {
   name: string;
   description: string;
@@ -8,4 +10,13 @@ export interface IProduct {
 
 export interface ICategory {
   category: string;
+  userId: string;
+}
+
+export interface IUser {
+  _id: mongoose.Types.ObjectId | null;
+  name: string;
+  image: string;
+  emailVerified: string | null;
+  email: string;
 }
