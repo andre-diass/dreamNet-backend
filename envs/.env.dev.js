a6c7ea39f7f595b63c64e91d7aff4a355d381dac
ANDRÉ DIAS S <adsf@poli.br>
2023-12-13 16:38:11 -0300

    fix/debug: add missing environment variable

--- envs/.env.dev.js
+++ envs/.env.dev.js
@@ -2,6 +2,7 @@
 const output = {
   STAGE: 'dev',
   DB: '${ssm:DEV_MONGODB_URI}',
+  JWTSECRET: '${ssm:DEV_JWTSECRET}',
 };
 
 module.exports = output;
--- envs/.env.prod.js
+++ envs/.env.prod.js
@@ -1,6 +1,7 @@
 const output = {
   STAGE: 'prod',
   DB: '${ssm:DEV_MONGODB_URI}',
+  JWTSECRET: '${ssm:DEV_JWTSECRET}',
 };
 
 module.exports = output;
