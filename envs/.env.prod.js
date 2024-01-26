a7968d7538faabd0858565e41d22a1b4bfc66dff
ANDRÉ DIAS S <adsf@poli.br>
2024-01-26 15:23:36 -0300

    test: create SQS resource

--- envs/.env.prod.js
+++ envs/.env.prod.js
@@ -2,6 +2,7 @@ const output = {
   STAGE: 'prod',
   DB: '${ssm:DEV_MONGODB_URI}',
   JWTSECRET: '${ssm:DEV_JWTSECRET}',
+  SQS_QUEUE_ACESSO_URL: { Ref: FilaAcessos },
 };
 
 module.exports = output;
