91ec225e2ef285a178ff8f9e755bf86405b56913
ANDRÉ DIAS S <adsf@poli.br>
2024-01-26 15:27:24 -0300

    fix/debug: Initialization error on deploy. FilaAcessos is not defined

--- envs/.env.dev.js
+++ envs/.env.dev.js
@@ -3,7 +3,7 @@ const output = {
   STAGE: 'dev',
   DB: '${ssm:DEV_MONGODB_URI}',
   JWTSECRET: '${ssm:DEV_JWTSECRET}',
-  SQS_QUEUE_ACESSO_URL: { Ref: FilaAcessos },
+  SQS_QUEUE_ACESSO_URL: '{ Ref: FilaAcessos }',
 };
 
 module.exports = output;
