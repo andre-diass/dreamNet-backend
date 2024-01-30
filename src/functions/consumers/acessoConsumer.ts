a19355740181d45d656b5ef2db76c69916521899
ANDRÉ DIAS S <adsf@poli.br>
2024-01-30 16:30:10 -0300

    feat: add email queue resource

--- src/functions/consumers/acessoConsumer.ts
+++ src/functions/consumers/acessoConsumer.ts
@@ -2,6 +2,9 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async evento => {
   console.log('DADOS EVENTO DO CONSUMER', evento);
+
+  // se eu jogar algum erro aqui. exemplo: uma chamada pra uma api externa dando errado, essa mensagem vai pra dlq, e lá eu posso olhar esse erro e debugar. lembrando que o erro pode vir de outra função mas tem que estourar aqui. então no meu catch aqui de chamada pra api eu vou só estourar o erro
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello from Lambda!' }),
